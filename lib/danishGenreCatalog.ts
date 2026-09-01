5b5564dd231c1be9047bac643c3c2ba55e36a9f2
atillisch-hue <a.tillisch@gmail.com>
2026-09-01 19:53:40 +0200

    Combine complete Danish genre catalog

---
+++ lib/danishGenreCatalog.ts
@@ -0,0 +1,8 @@
+import {danishGenres as baseGenres,danishGenreCategories,type DanishGenre,type DanishGenreCategory} from "./danishGenres";
+import {danishGenreExtras} from "./danishGenreExtras";
+
+export type {DanishGenre,DanishGenreCategory};
+export {danishGenreCategories};
+
+export const danishGenres:DanishGenre[]=[...baseGenres,...danishGenreExtras];
+export const danishGenreByName=(name:string)=>danishGenres.find(g=>g.name===name)||danishGenres.find(g=>g.id===name);
