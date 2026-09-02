04726e605e652f63a15a131d7368c86f4494a27c
atillisch-hue <a.tillisch@gmail.com>
2026-09-02 07:48:38 +0200

    Include remaining Danish genre variants

--- lib/danishGenreCatalog.ts
+++ lib/danishGenreCatalog.ts
@@ -1,8 +1,9 @@
 import {danishGenres as baseGenres,danishGenreCategories,type DanishGenre,type DanishGenreCategory} from "./danishGenres";
 import {danishGenreExtras} from "./danishGenreExtras";
+import {danishGenreMore} from "./danishGenreMore";
 
 export type {DanishGenre,DanishGenreCategory};
 export {danishGenreCategories};
 
-export const danishGenres:DanishGenre[]=[...baseGenres,...danishGenreExtras];
+export const danishGenres:DanishGenre[]=[...baseGenres,...danishGenreExtras,...danishGenreMore];
 export const danishGenreByName=(name:string)=>danishGenres.find(g=>g.name===name)||danishGenres.find(g=>g.id===name);
