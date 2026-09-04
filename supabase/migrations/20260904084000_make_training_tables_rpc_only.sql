bf233f898d065923dbf455f93e0f405608f43a85
atillisch-hue <a.tillisch@gmail.com>
2026-09-04 10:40:20 +0200

    Make training tables RPC-only

---
+++ supabase/migrations/20260904084000_make_training_tables_rpc_only.sql
@@ -0,0 +1,16 @@
+revoke all privileges on table
+  public.reading_exam_assignment_students,
+  public.reading_exam_assignments,
+  public.reading_exam_attempts,
+  public.reading_exam_sessions,
+  public.reading_strategy_assignment_students,
+  public.reading_strategy_assignments,
+  public.spelling_exam_assignment_students,
+  public.spelling_exam_assignments,
+  public.spelling_exam_attempts,
+  public.spelling_exam_sessions,
+  public.student_training_progress,
+  public.training_assignment_progress,
+  public.training_assignment_students,
+  public.training_assignments
+from authenticated, anon;
